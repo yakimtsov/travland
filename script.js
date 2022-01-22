37a203717e5ed446e30616e2abb05522667975c8
Yakimtsov2 <yakimtsov2@gmail.com>
2022-01-22 11:40:11 +0200

    add some updates

---
+++ script.js
@@ -0,0 +1,9 @@
+$('.offers__content').slick({
+	infinite: true,
+	slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    dots: true,
+    pauseOnHover:true,
+});
